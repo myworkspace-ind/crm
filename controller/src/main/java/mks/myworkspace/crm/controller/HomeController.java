1f5008e262bfa8c32b79f656da1a79e72083727a
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2024-10-10 22:52:54 +0700

    Adjust left navigation bar for page customerListCRMScreen.html

--- controller/src/main/java/mks/myworkspace/crm/controller/HomeController.java
+++ controller/src/main/java/mks/myworkspace/crm/controller/HomeController.java
@@ -57,12 +57,12 @@ public class HomeController extends BaseController {
 	 * Simply selects the home view to render by returning its name.
      * @return 
 	 */
-	@RequestMapping(value = {"/button-customize"}, method = RequestMethod.GET)
+	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
 	public ModelAndView displayHome(HttpServletRequest request, HttpSession httpSession) {
-		ModelAndView mav = new ModelAndView("buttonCustomize");
+		ModelAndView mav = new ModelAndView("homeCRMScreen");
 
 		initSession(request, httpSession);
-		log.debug("Button Customize Controller is running....");
+		log.debug("Home CRM Screen Controller is running....");
 		
 		mav.addObject("currentSiteId", getCurrentSiteId());
 		mav.addObject("userDisplayName", getCurrentUserDisplayName());
