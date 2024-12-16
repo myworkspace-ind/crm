92981d11880defe8dad8d64160f916e7914ddb78
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2024-12-16 14:08:25 +0700

    Review code UI Contact Page and and edit link at file hmenu.html to redirect to Contact Page.

--- controller/src/main/java/mks/myworkspace/crm/controller/ContactController.java
+++ controller/src/main/java/mks/myworkspace/crm/controller/ContactController.java
@@ -1,12 +1,38 @@
 package mks.myworkspace.crm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+//@Controller
+//public class ContactController {
+//	@GetMapping("/contact")
+//    public String showContactPage() {
+//        return "contact";
+//    }
+//}
 
 @Controller
-public class ContactController {
+@Slf4j	
+public class ContactController extends BaseController {
+	/**
+	 * Simply selects the home view to render by returning its name.
+     * @return Intro view 
+	 */
 	@GetMapping("/contact")
-    public String showContactPage() {
-        return "contact";
-    }
+	public ModelAndView displayContactPage(HttpServletRequest request, HttpSession httpSession) {
+		ModelAndView mav = new ModelAndView("contact");
+
+		initSession(request, httpSession);
+		
+		mav.addObject("currentSiteId", getCurrentSiteId());
+		mav.addObject("userDisplayName", getCurrentUserDisplayName());
+
+		return mav;
+	}
 }
