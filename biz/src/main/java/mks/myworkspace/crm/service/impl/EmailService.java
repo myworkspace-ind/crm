a0602d78f5834245fe687357fa825b6709f473b1
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2025-02-03 14:12:05 +0700

    Fix error of configuration's sending email successfully. Complete basic sending email (version 0).

--- biz/src/main/java/mks/myworkspace/crm/service/impl/EmailService.java
+++ biz/src/main/java/mks/myworkspace/crm/service/impl/EmailService.java
@@ -1,37 +1,39 @@
 package mks.myworkspace.crm.service.impl;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletContext;
+
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.internet.MimeMessage;
-
-import org.springframework.core.io.ClassPathResource;
-import java.nio.file.Files;
-
-import java.io.IOException;
-
 @Service
 public class EmailService {
     private final JavaMailSender mailSender;
+    private final ServletContext servletContext;
 
     // Constructor injection for JavaMailSender
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender, ServletContext servletContext) {
         this.mailSender = mailSender;
+        this.servletContext = servletContext;
     }
 
-    public void sendEmail(String to, String subject, String template, String location, String contactName, 
+    public void sendEmail(String to, String subject, String contactName, 
                           String contactNumber, String email, String companyName, String message, String address) throws Exception {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
     	MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-        // Đọc file template HTML
-        ClassPathResource resource = new ClassPathResource("emailTemplate.html");
-        String emailContent = new String(Files.readAllBytes(resource.getFile().toPath()));
+        
+    	//String filePath = "/crm-web/src/main/webapp/WEB-INF/templates/emailTemplate.html";
+    	//String emailContent = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+    	// Đọc file template từ thư mục WEB-INF
+        String filePath = servletContext.getRealPath("/WEB-INF/templates/emailTemplate.html");
+        String emailContent = new String(Files.readAllBytes(Paths.get(filePath)));
 
         // Thay thế các placeholder {{...}} bằng dữ liệu thực
-        emailContent = emailContent.replace("{{location}}", location)
-                                    .replace("{{contactName}}", contactName)
+        emailContent = emailContent .replace("{{contactName}}", contactName)
                                     .replace("{{contactNumber}}", contactNumber)
                                     .replace("{{email}}", email)
                                     .replace("{{companyName}}", companyName)
@@ -40,6 +42,8 @@ public class EmailService {
 
         // Cấu hình email
         helper.setTo(to);
+        helper.setFrom(email);
+        helper.setReplyTo(email);
         helper.setSubject(subject);
         helper.setText(emailContent, true); // Gửi email dưới dạng HTML
 
--- controller/src/main/java/mks/myworkspace/crm/controller/TrialRegistrationController.java
+++ controller/src/main/java/mks/myworkspace/crm/controller/TrialRegistrationController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
-//import mks.myworkspace.crm.service.impl.EmailService;
+import mks.myworkspace.crm.service.impl.EmailService;
 
 @Controller
 @Slf4j
 public class TrialRegistrationController extends BaseController {
-//	@Autowired
-//    private EmailService emailService;
+	@Autowired
+    private EmailService emailService;
 	
 	@GetMapping("/trial-registration")
 	public ModelAndView displayHome(HttpServletRequest request, HttpSession httpSession) {
@@ -31,22 +31,47 @@ public class TrialRegistrationController extends BaseController {
 		return mav;
 	}
 	
-//	@PostMapping("/send-email")
-//	public String sendEmail(@RequestParam String location,
-//							@RequestParam String contactName,
-//							@RequestParam String contactNumber,
-//                            @RequestParam String email,
-//                            @RequestParam String companyName,
-//                            @RequestParam String message,
-//                            @RequestParam String address) {
-//		try {
-//			emailService.sendEmail("crm.mekongsolution@gmail.com", "Thông tin đăng ký dùng thử mới từ khách hàng", "emailTemplate.html",
-//					location, contactName, contactNumber, email, companyName, message, address);
-//			return "redirect:/thankYou";
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//            return "redirect:/error"; // Redirect đến trang lỗi
-//		}
-//	}
+	@PostMapping("/send-email")
+	public String sendEmail(
+	                        @RequestParam String contactName,
+	                        @RequestParam String contactNumber,
+	                        @RequestParam String email,
+	                        @RequestParam String companyName,
+	                        @RequestParam String message,
+	                        @RequestParam String address) {
+	    try {
+	        emailService.sendEmail("crm.mekongsolution@gmail.com", 
+	                               "Thông tin đăng ký dùng thử mới từ khách hàng", 
+	                                contactName, contactNumber, email, companyName, message, address);
+	        return "redirect:/thank-you";
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "redirect:/error"; // Redirect đến trang lỗi
+	    }
+	}
+	
+	@GetMapping("/error")
+	public ModelAndView displayEmailError(HttpServletRequest request, HttpSession httpSession) {
+		ModelAndView mav = new ModelAndView("error");
+
+		initSession(request, httpSession);
+
+		mav.addObject("currentSiteId", getCurrentSiteId());
+		mav.addObject("userDisplayName", getCurrentUserDisplayName());
+
+		return mav;
+	}
+	
+	@GetMapping("/thank-you")
+	public ModelAndView displayEmailThanks(HttpServletRequest request, HttpSession httpSession) {
+		ModelAndView mav = new ModelAndView("thankYou");
+
+		initSession(request, httpSession);
+
+		mav.addObject("currentSiteId", getCurrentSiteId());
+		mav.addObject("userDisplayName", getCurrentUserDisplayName());
+
+		return mav;
+	}
 	
 }
