f6217cd8b831d326523efa344906811a5428fdd3
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2025-03-04 23:03:08 +0700

    Create a customer care table (just basic UI, not complete yet)

---
+++ web/src/main/webapp/resources/app-assets/js/update/chatAssistant.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const chatBtn = document.getElementById('chat-button');
+	const chatAssistant = document.getElementById('chat-assistant');
+	const modalContainer = document.getElementById('modal-container');
+	const modalOverlay = document.getElementById('modal-overlay');
+	const closeButton = document.getElementById('close-btn-modal');
+	const minimizeButton = document.getElementById('minimize-btn-modal');
+	let isMinimized = false;
+	
+	chatBtn.addEventListener('click', () => {
+		if (chatAssistant.style.display === 'none' || chatAssistant.style.display === '') {
+			chatAssistant.style.display = 'block';
+		} else {
+			chatAssistant.style.display = 'none';
+		}
+	})
+	
+	/*function toggleChat() {
+		const chat = document.getElementById('chat-assistant');
+		if (chat.style.display === 'none' || chat.style.display === '') {
+			chat.style.display = 'block';
+		} else {
+			chat.style.display = 'none';
+		}
+	}*/
+	
+	window.switchTab = function(tabId) {
+		const tabs = document.querySelectorAll('.tab-content');
+		tabs.forEach(tab => tab.style.display = 'none');
+
+		const activeTab = document.getElementById(tabId);
+		if (activeTab) {
+			activeTab.style.display = 'block';
+		}
+
+		const tabButtons = document.querySelectorAll('.tab-btn');
+		tabButtons.forEach(btn => btn.classList.remove('active'));
+
+		const clickedButton = document.querySelector(`button[onclick="switchTab('${tabId}')"]`);
+		if (clickedButton) {
+			clickedButton.classList.add('active');
+		}
+	};
+
+	const defaultTab = document.getElementById('customer-reminder');
+	if (defaultTab) {
+		switchTab('customer-reminder');
+	}
+	
+	//Mở customer care table
+	const clickHere = document.getElementById('click-here');
+	if (clickHere) {
+		clickHere.addEventListener('click', () => {
+			modalContainer.style.display = 'block';
+			modalOverlay.style.display = 'block';
+		});
+	}
+	
+	//Đóng customer care table
+	closeButton.addEventListener('click', () => {
+		console.log("Close button clicked!");
+		modalContainer.style.display = 'none';
+		modalOverlay.style.display = 'none';
+	});
+	
+	//Minimize customer care table
+	/*minimizeButton.addEventListener('click', () => {
+		if (!isMinimized) {
+			modalContainer.classList.add('minimzed');
+			minimizeButton.textContent = 'Restore';
+		} else {
+			modalContainer.classList.remove('minimized');
+			minimizeButton.textContent = 'Minimize';
+		}
+		isMinimized = !isMinimized
+	})*/
+	
+});
+
